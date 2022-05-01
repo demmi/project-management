cd8394d6a164c1a385ab3ca5f29e19c0b3da1f1a
Andrew Gerasimchik <gerasimchick@tut.by>
2022-05-01 23:11:28 +0300

    feat: create footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,23 +12,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
 import { AuthEffects } from './auth/store/effects/auth.effects';
+import { FooterComponent } from './core/pages/footer/footer.component';
+import {MatListModule} from "@angular/material/list";
 
 @NgModule({
   declarations: [
     AppComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    SharedModule,
-    StoreModule.forRoot(reducers, {
-      metaReducers,
-    }),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
-    EffectsModule.forRoot([AuthEffects]),
-    BrowserAnimationsModule,
-    AuthModule,
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        SharedModule,
+        StoreModule.forRoot(reducers, {
+            metaReducers,
+        }),
+        StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
+        EffectsModule.forRoot([AuthEffects]),
+        BrowserAnimationsModule,
+        AuthModule,
+        MatListModule,
+    ],
   providers: [],
   bootstrap: [AppComponent],
 })
