2b42e004620538c3fe5f353ccb5316c297f4e98e
Siarhei Danchanka <sdanchanka@exadel.com>
2022-05-03 18:41:24 +0300

    feat: add project-management module

---
+++ src/app/project-management/project-management.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { ProjectManagementRoutingModule } from './project-management-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    SharedModule,
+    ProjectManagementRoutingModule,
+  ],
+})
+export class ProjectManagementModule { }
