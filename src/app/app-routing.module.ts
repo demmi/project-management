6dcf92fd78605bb09f8f9f43b177d9f0d28cf3f0
Siarhei Danchanka <sdanchanka@exadel.com>
2022-05-04 21:00:25 +0300

    refactor: slit authentication into two pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginSignupPageComponent } from './auth/pages/login-signup-page/login-signup-page.component';
 import { PageNotFoundComponent } from './core/pages/page-not-found/page-not-found.component';
+import { LoginSignupPageComponent } from './auth/pages/login-signup-page/login-signup-page.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginSignupPageComponent },
+  { path: 'signup', component: LoginSignupPageComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
 
--- src/app/auth/pages/login-signup-page/login-signup-page.component.ts
+++ src/app/auth/pages/login-signup-page/login-signup-page.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-signup-page',
   templateUrl: './login-signup-page.component.html',
   styleUrls: ['./login-signup-page.component.scss'],
 })
-export class LoginSignupPageComponent {}
+export class LoginSignupPageComponent implements OnInit {
+
+  isLoginPage: boolean;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.isLoginPage = this.router.url.includes('/login');
+  }
+
+}
